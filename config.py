ac684d3587a4779a5e50ce9f65b397f292628adf
Volodimir <v.shinkarenko05@gmail.com>
2023-05-07 15:03:39 +0300

    add comments

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+#API URL
 url = "https://translate-plus.p.rapidapi.com/translate"
 
+#Headers
 headers = {
 	"content-type": "application/json",
 	"X-RapidAPI-Key": "02fbd95b13msha8c91940307c8fap10f6d0jsnb7cc0aad5df9",
--- translate_img.py
+++ translate_img.py
@@ -6,7 +6,7 @@ import PySimpleGUI as sg
 import io, os
 
 
-#
+#Path to tesseract(Only for Windows)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 sg.theme('dark grey 9')
@@ -14,10 +14,10 @@ sg.theme('dark grey 9')
 file_types = [("JPEG (*.jpg)", "*.jpg"),
               ("All files (*.*)", "*.*")]
 
+#Interface loyout
 layout = [
-	[sg.Image(key="-IMAGE-")],
 	[sg.Text("Choose your image")],
-    [sg.Input(key='-file-'), sg.FileBrowse(file_types=file_types, key='-FILE_BROWSE-')],
+    [sg.Input(key='-file-'), sg.FileBrowse(file_types=file_types)],
     [sg.OptionMenu(values=['en', 'uk', 'pl', 'fr', 'de'], default_value = 'en', key='-TEXT_LANG-'), sg.Push(),
     sg.OptionMenu(values=['en', 'uk', 'pl', 'fr', 'de'], default_value = 'en', key='-TRANSLATE_LANG-')],
     [sg.Multiline(size=(80, 15), key='-OUTPUT-')],
@@ -33,19 +33,7 @@ while True:
 	event, values = window.read()
 	if event == sg.WINDOW_CLOSED or event == 'Quit':
 		break
-	if event =='-FILE_BROWSE-':
-		filename = values["-file-"]
-		if os.path.exists(filename):
-			image = Image.open(values["-file-"])
-			image.thumbnail((400, 400))
-			bio = io.BytesIO()
 
-			# Actually store the image in memory in binary 
-			image.save(bio, format="PNG")
-
-			# Use that image data in order to 
-			window["-IMAGE-"].update(data=bio.getvalue())
-		
 	img_path = values['-file-']
 	img_file = f'{img_path}'
 
@@ -54,18 +42,22 @@ while True:
 	text_lang = values['-TEXT_LANG-']
 	translate_lang = values['-TRANSLATE_LANG-']
 
+	#Convert image text to str
 	result = pytesseract.image_to_string(img, lang=f'{text_lang}')
+
+	#API translate
 	payload = {
 	"text": f"{result}",
 	"source": f"{text_lang}",
 	"target": f"{translate_lang}"
 	}
 
-
+	#Get translation from json
 	response = requests.request("POST", url, json=payload, headers=headers)
 	data = response.json()
-
 	text = data['translations']['translation']
+
+	#Upload text on the screen
 	window['-OUTPUT-'].update(f'{text}')
 
 
